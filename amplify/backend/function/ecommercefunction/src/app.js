55b00211ae4bc3a6ad1e5cd2da508522437c6131
mmiatech <mmiatech@madisoncollege.edu>
2020-11-18 13:47:35 -0600

    configured cognito and verb functions...
    textbook get/post/delete methods

--- amplify/backend/function/ecommercefunction/src/app.js
+++ amplify/backend/function/ecommercefunction/src/app.js
@@ -15,31 +15,105 @@ See the License for the specific language governing permissions and limitations 
 	STORAGE_PRODUCTTABLE_NAME
 Amplify Params - DO NOT EDIT */
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+var express = require('express');
+var bodyParser = require('body-parser');
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+
+const AWS = require('aws-sdk');
+const { v4: uuid } = require('uuid');
+
+/* Cognito SDK */
+const cognito = new;
+AWS.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18'
+});
+
+/* Cognito User Pool ID
+*  This User Pool ID variable will be given to you by the CLI output after
+   adding the category
+*  This will also be available in the file itself, commented out at the top
+*/
+var userpoolId = process.env.AUTH_ECOMMERCEAPP0D70A185_USERPOOLID;
+
+// DynamoDB configuration
+const region = process.env.REGION;
+const ddb_table_name = process.env.STORAGE_PRODUCTTABLE_NAME;
+const docClient = new AWS.DynamoDB.DocumentClient({region});
 
 // declare a new express app
-var app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+var app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+
+async function getGroupsForUser(event) {
+  let userSub =
+    event
+      .requestContext
+      .identity
+      .cognitoAuthenticationProvider
+      .split(':CognitoSignIn:')[1]
+  let userParams = {
+    UserPoolId: userpoolId,
+    Filter: `sub = "${userSub}"`,
+  }
+  let userData = await cognito.listUsers(userParams).promise()
+  const user = userData.Users[0]
+  var groupParams = {
+    UserPoolId: userpoolId,
+    Username: user.Username
+  }
+  const groupData = await cognito.adminListGroupsForUser(groupParams).promise()
+  return groupData
+}
+
+async function canPerformAction(event, group) {
+  return new Promise(async (resolve, reject) => {
+    if (!event.requestContext.identity.cognitoAuthenticationProvider) {
+      return reject()
+    }
+    const groupData = await getGroupsForUser(event)
+    const groupsForUser = groupData.Groups.map(group => group.GroupName)
+    if (groupsForUser.includes(group)) {
+      resolve()
+    } else {
+      reject('user not in group, cannot perform action..')
+    }
+  })
+}
 
 
 /**********************
  * Example get method *
  **********************/
 
-app.get('/products', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
+app.get('/products', async function(req, res) {
+  try {
+    const data = await getItems();
+    res.json({ data: data });
+  } catch (err) {
+    res.json({ error: err });
+  }
+})
+
+async function getItems(){
+  var params = { TableName: ddb_table_name }
+  try {
+    const data = await docClient.scan(params).promise();
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
 
 app.get('/products/*', function(req, res) {
   // Add your code here
@@ -50,9 +124,21 @@ app.get('/products/*', function(req, res) {
 * Example post method *
 ****************************/
 
-app.post('/products', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+app.post('/products', async function(req, res) {
+  const { body } = req;
+  const { event } = req.apiGateway;
+  try {
+    await canPerformAction(event, 'Admin');
+    const input = { ...body, id: uuid() };
+    var params = {
+      TableName: ddb_table_name,
+      Item: input
+    };
+    await docClient.put(params).promise();
+    res.json({ success: 'item saved to database..' });
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 
 app.post('/products/*', function(req, res) {
@@ -78,9 +164,19 @@ app.put('/products/*', function(req, res) {
 * Example delete method *
 ****************************/
 
-app.delete('/products', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+app.delete('/products', async function(req, res) {
+  const { event } = req.apiGateway
+  try {
+    await canPerformAction(event, 'Admin');
+    var params = {
+      TableName : ddb_table_name,
+      Key: { id: req.body.id }
+    };
+    await docClient.delete(params).promise();
+    res.json({ success: 'successfully deleted item' });
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 
 app.delete('/products/*', function(req, res) {
