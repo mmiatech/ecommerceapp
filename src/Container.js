914b70b0126ef8d405875622cb0d742fe9dc16f2
mmiatech <mmiatech@madisoncollege.edu>
2020-11-23 12:52:04 -0600

    container reformatted..
    return default- if using lambda, have to put return at bottom

--- src/Container.js
+++ src/Container.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-export default function Container({ children }) {
+const Container = ({ children }) => {
   return (
     <div style={containerStyle}>
       {children}
     </div>
-  )
+  );
 }
 
 const containerStyle = {
-  width: 900,
-  margin: '0 auto',
-  padding: '20px 0px'
+  width: 900
+  , margin: '0 auto'
+  , padding: '20px 0px'
 }
+
+export default Container;
