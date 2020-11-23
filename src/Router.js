4e9c8816fc375761c77d6e462a79f851098cc703
mmiatech <mmiatech@madisoncollege.edu>
2020-11-23 13:47:30 -0600

    reformatted router.js

--- src/Router.js
+++ src/Router.js
@@ -6,28 +6,56 @@ import Admin from './Admin';
 import Main from './Main';
 import Profile from './Profile';
 
-export default function Router() {
-  const [current, setCurrent] = useState('home')
-  useEffect(() => {
-    setRoute()
-    window.addEventListener('hashchange', setRoute)
-    return () =>  window.removeEventListener('hashchange', setRoute)
-  }, [])
-  function setRoute() {
-    const location = window.location.href.split('/')
-    const pathname = location[location.length-1]
-    console.log('pathname: ', pathname)
-    setCurrent(pathname ? pathname : 'home')
+const Router = () => {
+  const [current, setCurrent] = useState('home');
+
+  useEffect(
+    () => {
+      setRoute();
+      window.addEventListener('hashchange', setRoute);
+
+      return () =>  window.removeEventListener('hashchange', setRoute);
+    }
+    , []
+  );
+
+  const setRoute = () => {
+    //get location from window object
+    const location = window.location.href.split('/');
+
+    //path is the last part of location, location is an arry of strings
+    const pathname = location[location.length-1];
+    console.log('pathname: ', pathname);
+
+    setCurrent(pathname ? pathname : 'home');
   }
+
   return (
     <HashRouter>
       <Nav current={current} />
+
       <Switch>
-        <Route exact path='/' component={Main} />
-        <Route path='/admin' component={Admin} />
-        <Route path='/profile' component={Profile} />
-        <Route component={Main} />
+        <Route
+          exact path='/'
+          component={Main}
+        />
+
+        <Route
+          path='/admin'
+          component={Admin}
+        />
+
+        <Route
+          path='/profile'
+          component={Profile}
+        />
+
+        <Route
+          component={Main}
+        />
       </Switch>
     </HashRouter>
-  )
+  );
 }
+
+export default Router;
